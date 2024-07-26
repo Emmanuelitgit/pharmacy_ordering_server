6a7b5cc523c162a5e4224a3f60b768e5d619371d
Emmanuelitgit <eyidana001@st.ug.edu.gh>
2024-07-26 15:37:31 +0000

    connection

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ dotenv.config();
 mongoose 
  .connect(process.env.CONNECTION_STRING, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,   })   
+        useUnifiedTopology: true, })   
  .then(() => console.log("Database connected!"))
  .catch(err => console.log(err));
 
